b1f12d66285bf31263ca8a1bb8c6050ed604f45d
eduardahey <cavalcantemeds@gmail.com>
2024-10-05 08:46:38 -0300

    Initial commit

---
+++ src/services/login.test.tsx
@@ -0,0 +1,16 @@
+import { login } from "./login"
+
+describe('login', () => {
+
+    const mockEmail = 'nath@dio.bank'
+    const mockPassword = '123456'
+    it('Deve exibir um alert com boas vindas caso o email e senha seja válido', async() => {
+        const response = await login(mockEmail,mockPassword)
+        expect(response).toBeTruthy()
+    })
+
+    it('Deve exibir um erro caso o email seja inválido', async() => {
+        const response = await login('email@invalido.com','123')
+        expect(response).toBeFalsy()
+    })
+})
